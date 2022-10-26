f6fd4e29137b2607a5aabd70dfed438687581019
haminhduc <haminhduc407@mail.com>
2022-10-26 13:00:13 +0700

    display shuffled answers

--- src/testPage.js
+++ src/testPage.js
@@ -20,12 +20,13 @@ function TestPage() {
 
       const allAnswers = [...quiz.incorrect_answers];
       allAnswers.push(quiz.correct_answer);
-      console.log(allAnswers);
+      shuffle(allAnswers);
+      allAnswers.forEach((answer) => {
+        answerSpan.textContent += decode(answer) + " ---  ";
+      });
       quizzBlock.append(newQuestion, answerSpan);
     });
   }
-  getQuestions(handleQuestionDisplay);
-  return <div className="test"></div>;
 
   // handle decode special characters
   function decode(str) {
@@ -36,25 +37,27 @@ function TestPage() {
     return txt.value;
   }
   // shuffle answers
-  // function shuffle(array) {
-  //   let currentIndex = array.length,
-  //     randomIndex;
+  function shuffle(array) {
+    let currentIndex = array.length,
+      randomIndex;
 
-  //   // While there remain elements to shuffle.
-  //   while (currentIndex !== 0) {
-  //     // Pick a remaining element.
-  //     randomIndex = Math.floor(Math.random() * currentIndex);
-  //     currentIndex--;
+    // While there remain elements to shuffle.
+    while (currentIndex !== 0) {
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
 
-  //     // And swap it with the current element.
-  //     [array[currentIndex], array[randomIndex]] = [
-  //       array[randomIndex],
-  //       array[currentIndex],
-  //     ];
-  //   }
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex],
+        array[currentIndex],
+      ];
+    }
 
-  //   return array;
-  // }
+    return array;
+  }
+  getQuestions(handleQuestionDisplay);
+  return <div className="test"></div>;
 }
 
 export default TestPage;
